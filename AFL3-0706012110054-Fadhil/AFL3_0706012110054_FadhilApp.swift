8c685d2186d73893525320468a79b990a35beade
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-04-11 08:28:22 +0700

    Initial Commit

---
+++ AFL3-0706012110054-Fadhil/AFL3_0706012110054_FadhilApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110054_FadhilApp.swift
+//  AFL3-0706012110054-Fadhil
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110054_FadhilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
