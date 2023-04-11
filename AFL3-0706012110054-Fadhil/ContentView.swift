a2648ff3f2527427b5f2f006166cd7f4cb7c09e7
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-04-11 08:31:57 +0700

    Section 1

--- AFL3-0706012110054-Fadhil/ContentView.swift
+++ AFL3-0706012110054-Fadhil/ContentView.swift
@@ -9,15 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, SwiftUI!")
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
