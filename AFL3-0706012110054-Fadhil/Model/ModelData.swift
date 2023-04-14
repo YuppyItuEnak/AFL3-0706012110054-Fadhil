eb058b80545fbaa00756f4b20eb1fd4e26e65154
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-04-14 09:09:58 +0700

    Section 4 Ch 1 pt 3

--- AFL3-0706012110054-Fadhil/Model/ModelData.swift
+++ AFL3-0706012110054-Fadhil/Model/ModelData.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+import Combine
+
+final class ModelData: ObservableObject{
+  @Published  var landmarks: [Landmark] = load("landmarkData.Json")
+}
 
 var landmarks: [Landmark] = load("landmarkData.json")
 
