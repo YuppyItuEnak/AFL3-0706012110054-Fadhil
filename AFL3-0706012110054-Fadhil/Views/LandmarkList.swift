5ed97f07a47df797ab94974ace79369ce4edba9d
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-04-14 09:03:56 +0700

    Section 3 Ch1 Pt 3

--- AFL3-0706012110054-Fadhil/Views/LandmarkList.swift
+++ AFL3-0706012110054-Fadhil/Views/LandmarkList.swift
@@ -9,9 +9,17 @@ import Foundation
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = true
+    
+    var filteredLandmarks: [Landmark]{
+        landmarks.filter{ landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView{
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink{
                     LandmarkDetail(landmark: landmark)
                 }label: {
