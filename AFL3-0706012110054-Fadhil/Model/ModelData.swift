91fc9a7332742d84f308d82d3c0631c3748d5cb7
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-04-14 14:31:12 +0700

    Chapter 3 Done

--- AFL3-0706012110054-Fadhil/Model/ModelData.swift
+++ AFL3-0706012110054-Fadhil/Model/ModelData.swift
@@ -11,11 +11,22 @@ import Combine
 final class ModelData: ObservableObject{
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
+    
+    var features: [Landmark]{
+        landmarks.filter{$0.isFeatured}
+    }
+    
+    var categories: [String: [Landmark]]{
+        Dictionary(
+            grouping: landmarks, by: { $0.category.rawValue }
+        )
+    }
 }
 
-var landmarks: [Landmark] = load("landmarkData.json")
+    var landmarks: [Landmark] = load("landmarkData.json")
 
-func load<T: Decodable>(_ filename: String) -> T {
+    func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
